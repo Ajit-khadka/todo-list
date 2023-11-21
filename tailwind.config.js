0d237fdca15976d1ad39207ca38bbcf272ff498a
Ajit-khadka <khadkaajit34@gmail.com>
2023-11-21 11:35:07 +0545

    deploy changs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'Black' : '#222831',
+      'Grey' : '#393E46',
+      'Teal' : '#00ADB5',
+      'Space': '#EEEEEE',
+      
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      fontFamily: {
+        roboto : ['Roboto Mono', 'monospace'],
+      }
     },
   },
   plugins: [],
-}
+};
